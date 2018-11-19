dffda50eee873536642a4946c9cf6ce37c57b35a
michael haberman <michaelhaberman@michaels-MacBook-Pro.local>
2018-11-19 19:27:56 +0200

    config and token

--- src/IMCRSubmitRecordComponent.js
+++ src/IMCRSubmitRecordComponent.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import {getRecord} from './mitraClipRecord';
 import {api} from "./config";
+const cookies = require('browser-cookies');
+
 
 
 export function SubmitRecord(){
@@ -8,11 +10,12 @@ export function SubmitRecord(){
 }
 
 function sendRecord(){
-    fetch(`${api}/record`, {
+    fetch(`${api}/record?token=${cookies.get("imcr-token")}`, {
         method:'post',
         credentials: 'include',
         headers: {
-            'Content-Type': 'application/json'
+            'Content-Type': 'application/json',
+            'Cookie':document.cookie
           },
         body: JSON.stringify(getRecord())
     }).then((res)=>{
